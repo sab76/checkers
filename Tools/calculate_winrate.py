0f150a8ed44bdc42df26df140aa797abb8b9e471
Sabine Nicolle <snicolle@uci.edu>
2023-11-12 23:02:16 -0800

    bbbb

--- Tools/calculate_winrate.py
+++ Tools/calculate_winrate.py
@@ -1,4 +1,5 @@
 import subprocess
+import concurrent.futures
 
 def run_script_and_get_output():
     result = subprocess.run(["python3", "AI_Runner.py", "8", "8", "3", "l", "../src/checkers-python/main.py", "./Sample_AIs/Random_AI/main.py"], capture_output=True, text=True)
@@ -6,14 +7,19 @@ def run_script_and_get_output():
 
 def calculate_win_rate():
     player1_wins = 0
+    total_games = 100
 
-    for _ in range(100):
-        output = run_script_and_get_output()
-        lines = output.split('\n') # Split output into lines
-        if lines and ("player 1 wins" in lines[-1] or "Tie" in lines[-1]): # Check last line
-            player1_wins += 1
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Submit all games to be run in parallel
+        futures = [executor.submit(run_script_and_get_output) for _ in range(total_games)]
 
-    win_rate = player1_wins / 100
+        for future in concurrent.futures.as_completed(futures):
+            output = future.result()
+            lines = output.split('\n')
+            if lines and ("player 1 wins" in lines[-1] or "Tie" in lines[-1]):
+                player1_wins += 1
+
+    win_rate = player1_wins / total_games
     return win_rate
 
 # Run the win rate calculation
