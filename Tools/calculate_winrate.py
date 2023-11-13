79645354b79875cd423d3c5290fb2cce1b1f80b5
Sabine Nicolle <snicolle@uci.edu>
2023-11-12 22:57:52 -0800

    kjhkjh

--- Tools/calculate_winrate.py
+++ Tools/calculate_winrate.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def run_script_and_get_output():
-    result = subprocess.run(["python3", "AI_Runner.py 8 8 3 l ../src/checkers-python/main.py ./Sample_AIs/Random_AI/main.py"], capture_output=True, text=True)
+    result = subprocess.run(["python3", "AI_Runner.py", "8", "8", "3", "l", "../src/checkers-python/main.py", "./Sample_AIs/Random_AI/main.py"], capture_output=True, text=True)
     return result.stdout.strip()
 
 def calculate_win_rate():
@@ -9,7 +9,8 @@ def calculate_win_rate():
 
     for _ in range(100):
         output = run_script_and_get_output()
-        if "player 1 wins" in output or "Tie" in output:
+        lines = output.split('\n') # Split output into lines
+        if lines and ("player 1 wins" in lines[-1] or "Tie" in lines[-1]): # Check last line
             player1_wins += 1
 
     win_rate = player1_wins / 100
